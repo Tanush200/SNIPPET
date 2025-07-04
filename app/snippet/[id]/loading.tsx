f29b39b2b12108c75210959adb4c2074ec5ddb77
Tanush Saha <tanush.saha05@gmail.com>
2025-07-04 19:11:05 +0530

    done

--- app/snippet/[id]/loading.tsx
+++ app/snippet/[id]/loading.tsx
@@ -4,7 +4,7 @@ const Loading = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white-100 text-white">
       <div className="flex flex-col items-center gap-4">
-        <div className="w-12 h-12 border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+        <div className="w-12 h-12 border-4 border-gray-200 border-t-yellow-500 rounded-full animate-spin"></div>
         <h1 className="text-lg font-semibold tracking-wide animate-pulse">
           Loading, please wait...
         </h1>
